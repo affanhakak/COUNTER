3ea74bca168881346208b72094d82f3f543b9031
Affan Showkat Hakak <affanshowkat@gmail.com>
2022-01-26 20:48:23 +0530

    mm

--- counter.js
+++ counter.js
@@ -1,21 +1,31 @@
-const button1 = document.querySelector(".button1");
-const button2 = document.querySelector(".button2");
-const reset = document.querySelector(".reset");
-const score = document.querySelector(".score");
+const button1 =
+  document.querySelector("#button1");
+const button2 =
+  document.querySelector("#button2");
+const reset =
+  document.querySelector("#reset");
+const score =
+  document.querySelector("#score");
 
 let count = 0;
 
-button1.addEventListener("click", (e) => {
-  count += 1;
-  score.textContent = count;
-  score.style.color = "green";
-});
+button1.addEventListener(
+  "click",
+  (e) => {
+    count += 1;
+    score.textContent = count;
+    score.style.color = "green";
+  }
+);
 
-button2.addEventListener("click", () => {
-  count -= 1;
-  score.textContent = count;
-  score.style.color = "red";
-});
+button2.addEventListener(
+  "click",
+  () => {
+    count -= 1;
+    score.textContent = count;
+    score.style.color = "red";
+  }
+);
 
 reset.addEventListener("click", () => {
   score.textContent = 0;
